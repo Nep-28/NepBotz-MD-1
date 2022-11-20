18aba5c1ec73a7268944f8ec1ad7f1613f2ec57c
Nep-28 <107740473+Nep-28@users.noreply.github.com>
2022-11-20 12:02:23 +0530

    Update EN.js

--- Language/EN.js
+++ Language/EN.js
@@ -16,11 +16,12 @@ neo.reply = {
     ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
     nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
     banChat: 'The bot was banned in this group, please contact the owner to unban',
-    attack: 'Type the number u want to attack',
-    kick: 'User has been successfully removed from this group',
-    add: 'User has been successfully added to this group',
-    promote: 'User successfully promoted',
-    demote: 'User successfully demoted',
-    block: 'Blocked',
-    unblock: 'Unblocked' 
+    attack: 'Type the number you  want to attack',
+    kick: '⚙️ User has been successfully removed from this group ✔️',
+    add: '⚙️ User has been successfully added to this group ✔️',
+    promote: '⚙️ User successfully promoted ✔️',
+    demote: '⚙️ User successfully demoted ✔️',
+    block: 'Blocked ✔️',
+    unblock: 'Unblocked ✔️',
+    sticker: 'Creating the sticker 🧰'
 }
